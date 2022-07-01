8558e3283c2def7057e5005ae27610b3c2c63ba6
Katsiaryna Kravets <Katsiaryna.Kravets@sabre.com>
2022-07-01 15:56:05 +0200

    dummy commit

--- src/components/CurrentTemperature.js
+++ src/components/CurrentTemperature.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 
 const CurrentTemperature = () => {
+    // comment
 
     const [temperature, setTemperature] = useState(null);
     const [loading, setLoading] = useState(false);
